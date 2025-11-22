e6db3eb8865d83a04fc19fcec09610c82202deb1
AbhiMalloy <abhishek.saxena@malloyaeronautics.com>
2025-11-22 21:52:31 +0000

    Formatting the file

--- src/libs/chatbot_apis.py
+++ src/libs/chatbot_apis.py
@@ -4,31 +4,29 @@ from datetime import datetime
 from playsound import playsound
 from pathlib import Path
 
-class ChatbotAPI():
-    '''Base class for chatbot APIs.'''
+
+class ChatbotAPI:
+    """Base class for chatbot APIs."""
 
     def __init__(self, client, model: str):
-        '''Initialise the Chatbot with the client to openai'''
+        """Initialise the Chatbot with the client to openai"""
         self.client = client
         # Context defines the behaviour of the chatbot
         # There are 3 parts to communicating: system, user and assistant
-        self.context = [
-            {"role": "system", "content": "You are a helpful assistant."}
-        ]
+        self.context = [{"role": "system", "content": "You are a helpful assistant."}]
         # this is the model for the chatbot defined by the user
         self.model = model
 
 
     async def send_message(self, message: str):
-        '''Send a message to the chatbot API.'''    
+        """Send a message to the chatbot API."""
         # Here the message is coming from the user
         # Appending the context with the user message
         self.context.append({"role": "user", "content": message})
         try:
             # Sending the message to the openai client
             response = self.client.chat.completions.create(
-                model=self.model,
-                messages=self.context
+                model=self.model, messages=self.context
             )
             # print(f"Response received: {response.choices[0].message.content}")
             # Getting the response from the assistant
@@ -41,13 +39,14 @@ class ChatbotAPI():
         except OpenAIError as e:
             print("An error occurred while communicating with the OpenAI API.")
     def print_chat(self, response_text: str):
-        '''Print the latest chat response.'''
+        """Print the latest chat response."""
         print(f"Assistant: {response_text}")
-    
+
     def print_chat_history(self):
-        '''Print the chat history.'''
+        """Print the chat history."""
         for message in self.context:
-            if message['role'] == "user":
+            if message["role"] == "user":
                 print(f"User: {message['content']}")
-            elif message['role'] == "assistant":
+            elif message["role"] == "assistant":
                 print(f"Assistant: {message['content']}")
+
