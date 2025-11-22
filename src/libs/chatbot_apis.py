6c29e72711fa9bec0bec085caa2c752206fea60d
Abhishek Saxena <abhishek.saxena.mit@gmail.com>
2025-11-22 16:51:14 +0000

    Adding the Chatbot class with conv api

---
+++ src/libs/chatbot_apis.py
@@ -0,0 +1,48 @@
+from openai import AuthenticationError, OpenAIError
+import asyncio
+
+class ChatbotAPI():
+    '''Base class for chatbot APIs.'''
+
+    def __init__(self, client, model: str):
+        '''Initialise the Chatbot with the client to openai'''
+        self.client = client
+        # Context defines the behaviour of the chatbot
+        # There are 3 parts to communicating: system, user and assistant
+        self.context = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+        # this is the model for the chatbot defined by the user
+        self.model = model
+
+
+    async def send_message(self, message: str):
+        '''Send a message to the chatbot API.'''    
+        # Here the message is coming from the user
+        # Appending the context with the user message
+        self.context.append({"role": "user", "content": message})
+        try:
+            # Sending the message to the openai client
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=self.context
+            )
+            # print(f"Response received: {response.choices[0].message.content}")
+            # Getting the response from the assistant
+            response_content = response.choices[0].message.content
+
+            # Appending the context with the assistant response to maintain the conversation
+            self.context.append({"role": "assistant", "content": response_content})
+        except AuthenticationError as e:
+            raise AuthenticationError("Authentication failed. Please check your API key.") from e
+        except OpenAIError as e:
+            raise OpenAIError("An error occurred while communicating with the OpenAI API.") from e
+        await self.print_chat_history()
+
+    async def print_chat_history(self):
+        '''Print the chat history.'''
+        for message in self.context:
+            if message['role'] == "user":
+                print(f"User: {message['content']}")
+            elif message['role'] == "assistant":
+                print(f"Assistant: {message['content']}")
