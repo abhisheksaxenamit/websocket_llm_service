7cce350b5cf6d9a4a51fc6039ba254fef1feac27
AbhiMalloy <abhishek.saxena@malloyaeronautics.com>
2025-11-24 22:28:08 +0000

    Adding log message to server main

--- src/websocket_server_main.py
+++ src/websocket_server_main.py
@@ -2,17 +2,19 @@ import asyncio
 from libs.websocket_server_apis import WebSocketServerAPI
 from libs.common_functions import load_config_from_json
 from pathlib import Path
+from libs.common_functions import log_message
 
 async def main():
     # get the server configuration
     config_path = Path(__file__).parent / 'libs' / 'configs' / 'server_config.json'
     config_args = load_config_from_json(config_path)
-    print(f"Loaded server configuration: {config_args.server_host}:{config_args.server_port }")
+    log_message("Server", f"Loaded server configuration: {config_args.server_host}:{config_args.server_port }")
     try:
         server = WebSocketServerAPI(host=config_args.server_host, port=config_args.server_port, config_args=config_args)
         await server.start()
     except Exception as e:
         print(f"Error starting server: {e}")
+        log_message("Server", f"Error starting server: {e}", level='error')
 
 if __name__ == "__main__":
     asyncio.run(main())
